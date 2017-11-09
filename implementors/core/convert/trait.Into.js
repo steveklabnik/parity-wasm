798fbd56fab3eac3b7cc9f6fdbb0585ab56f9d55
Travis CI User <travis@example.org>
2017-11-09 22:03:48 +0000

    Update documentation

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["parity_wasm"] = ["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"parity_wasm/interpreter/enum.Error.html\" title=\"enum parity_wasm::interpreter::Error\">Error</a>&lt;E&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;E: <a class=\"trait\" href=\"parity_wasm/interpreter/trait.UserError.html\" title=\"trait parity_wasm::interpreter::UserError\">UserError</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
